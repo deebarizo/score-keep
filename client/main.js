ab0f85173feaa4ca144e8c67515564e22ac15bc7
Dee Barizo <deebarizo@gmail.com>
2018-01-19 17:12:40 -0600

    Add increment and decrement features

--- client/main.js
+++ client/main.js
@@ -10,7 +10,15 @@ const renderPlayers = (players) => {
 		return (
 			<p key={player._id}>
 				{player.name} has {player.score} point(s).
-				<button onClick={() => Players.remove({ "_id": player._id }) }>X</button>
+				<button onClick={() => {
+					Players.update(player._id, {$inc: {"score": 1}});
+				}}>+1</button>
+				<button onClick={() => {
+					Players.update(player._id, {$inc: {"score": -1}}); 
+				}}>-1</button>
+				<button onClick={() => {
+					Players.remove(player._id);
+				}}>X</button>
 			</p>
 		);
 	});
