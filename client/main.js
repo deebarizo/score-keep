3ac5dd262705e2718b6c8efa3c8c8b6f46e87da7
Dee Barizo <deebarizo@gmail.com>
2018-01-20 15:34:47 -0600

    Refactor with React components

--- client/main.js
+++ client/main.js
@@ -4,59 +4,35 @@ import {Meteor} from 'meteor/meteor';
 import {Tracker} from 'meteor/tracker';
 
 import {Players} from './../imports/api/players';
+import TitleBar from './../imports/ui/TitleBar';
+import AddPlayer from './../imports/ui/AddPlayer';
+import Player from './../imports/ui/Player';
 
 const renderPlayers = (players) => {
 	return players.map((player) => {
 		return (
-			<p key={player._id}>
-				{player.name} has {player.score} point(s).
-				<button onClick={() => {
-					Players.update(player._id, {$inc: {"score": 1}});
-				}}>+1</button>
-				<button onClick={() => {
-					Players.update(player._id, {$inc: {"score": -1}}); 
-				}}>-1</button>
-				<button onClick={() => {
-					Players.remove(player._id);
-				}}>X</button>
-			</p>
+			<Player 
+				key={player._id}
+				player={player}
+			/>
 		);
 	});
-};
-
-const handleSubmit = (e) => {
-	let playerName = e.target.playerName.value;
-	
-	e.preventDefault();
-
-	if (playerName) {
-		e.target.playerName.value = '';
-
-		Players.insert({
-			name: playerName,
-			score: 0
-		});
-	}
 };
 
 Meteor.startup(() => {
 	Tracker.autorun(() => {
 		let players = Players.find().fetch();
 
-		let title = 'Score Keep';
-		let name = 'Dee';
 		let jsx = (
 			<div>
-				<h1>{title}</h1>
-				<p>Hello {name}!</p>
-				<p>This is my second paragraph.</p>
+				<TitleBar />
+
 				{renderPlayers(players)}
-				<form onSubmit={handleSubmit}>
-					<input type="text" name="playerName" placeholder="Player Name" />
-					<button>Add Player</button>
-				</form>
+				
+				<AddPlayer />
 			</div>
 		);
+
 		ReactDOM.render(jsx, document.getElementById('app'));
 	});
 });
