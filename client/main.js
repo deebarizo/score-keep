2684bb2de11db5b89b5efc45ca0dbdb913e409ef
Dee Barizo <deebarizo@gmail.com>
2018-01-21 15:26:06 -0600

    Sort players by score

--- client/main.js
+++ client/main.js
@@ -8,7 +8,11 @@ import App from './../imports/ui/App';
 
 Meteor.startup(() => {
 	Tracker.autorun(() => {
-		let players = Players.find().fetch();
+		let players = Players.find({},{
+			sort: {
+				score: -1
+			}
+		}).fetch();
 
 		ReactDOM.render(<App players={players} />, document.getElementById('app'));
 	});
